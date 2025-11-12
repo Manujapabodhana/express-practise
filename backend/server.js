8e087897c471e153d1b06998e383880a79451ff0
Manujapabodhana <rpmanuja123@gmail.com>
2025-11-12 20:47:31 +0530

    q

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get('/api/contacts', (req, res) => {
+    res.status(200).json({ message: 'Get all contacts' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
